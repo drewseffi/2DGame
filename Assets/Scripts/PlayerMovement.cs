eb33ff46d8ab1a208e366eca4cb731e0912d133c
drewseffi <drewsutherland23@gmail.com>
2025-09-10 04:57:00 +0100

    Add player walk animations and dash ability Heart UI art added as well

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody2D rb;
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private Rigidbody2D rb;
 
 	private Vector2 moveDir;
 
+	[SerializeField] private float dashForce;
+	[SerializeField] private float dashTime;
+	[SerializeField] private float dashCooldown;
+	private bool isDashing;
+	private bool canDash = true;
+
+	[SerializeField] private Animator anim;
+	[SerializeField] private SpriteRenderer sr;
+
+	private bool horiz;
+
 	void Update()
 	{
-		ProcessInputs();
-		Move();
+		if (!isDashing)
+			ProcessInputs();
+
+		if (Input.GetKeyDown(KeyCode.Space) && canDash && moveDir != Vector2.zero)
+		{
+			StartCoroutine(Dash());
+		}
 	}
 
 	void FixedUpdate()
 	{
-		
+		if (!isDashing)
+		{
+			Move();
+		}
 	}
 
 	void ProcessInputs()
@@ -27,11 +47,69 @@ public class PlayerMovement : MonoBehaviour
 		float moveY = Input.GetAxisRaw("Vertical");
 
 		moveDir = new Vector2(moveX, moveY).normalized;
+
+
+		// Horizontal movement
+		switch (moveDir.x)
+		{
+			// Moving right
+			case > 0:
+				anim.SetBool("RunHor", true);
+				sr.flipX = false;
+				horiz = true;
+				break;
+			// Moving left
+			case < 0:
+				anim.SetBool("RunHor", true);
+				sr.flipX = true;
+				horiz = true;
+				break;
+			// Stopped
+			case 0:
+				anim.SetBool("RunHor", false);
+				sr.flipX = false;
+				horiz = false;
+				break;
+		}
+
+		// Vertical movement
+		if (!horiz)
+		{
+			switch (moveDir.y)
+			{
+				case > 0:
+					anim.SetBool("RunUp", true);
+					anim.SetBool("RunDown", false);
+					break;
+				case < 0:
+					anim.SetBool("RunDown", true);
+					anim.SetBool("RunUp", false);
+					break;
+				case 0:
+					anim.SetBool("RunUp", false);
+					anim.SetBool("RunDown", false);
+					break;
+			}
+		}
 	}
 
 	void Move()
 	{
-		rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+		rb.velocity = moveDir * moveSpeed;
 	}
 
+	IEnumerator Dash()
+	{
+		isDashing = true;
+		canDash = false;
+
+		rb.velocity = moveDir * dashForce;
+
+		yield return new WaitForSeconds(dashTime);
+
+		isDashing = false;
+
+		yield return new WaitForSeconds(dashCooldown);
+		canDash = true;
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# 2DGame
+# 2D Top Down Game
+
+## Aims
+
+I want this game to be a top down game with art inspired by classic Gameboy games like Pokemon and Zelda with the "souls-like" dodge system and bosses with multiple phases and attack patterns. This will be a large undertaking and will just be a side project for me to get better as a game dev and to develop my skills where they are lacking (art, 2D games and in-depth enemy mechanics).
+
+## Art Style
+
+As mentioned above this game should remind the player of old-school games. To do this I will use 16x16px sprites and a very limited colour palette. I found a palette online called Velvet Cherry GB (available here https://lospec.com/palette-list/velvet-cherry-gb) that I really like the look of as I love the colour purple. This limited colour palette and small sprite sizes will hopefully meet my aims of a retro style while still being appealing to modern audiences.
